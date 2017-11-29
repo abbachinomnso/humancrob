d4fa8a3827e07e3933ad7d873f9dfa18896b2dbf
abbachinomnso <abbachinomnso@gmail.com>
2017-11-29 01:22:38 +0000

    working web app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+//import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { contentHeaders } from './common/common.component';
 
+import { MedicationComponent } from './medication/medication.component';
+
+const appRoutes: Routes = [
+  { path: '',       component: AppComponent  },
+  { path: 'medication',       component: MedicationComponent },
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    MedicationComponent
+    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
---
+++ src/app/medication/medication.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Http } from '@angular/http';
+import { contentHeaders } from '../common/common.component';
+@Component({
+  selector: 'app-medication',
+  templateUrl: './medication.component.html',
+  styleUrls: ['./medication.component.css']
+})
+export class MedicationComponent implements OnInit {
+
+  constructor(public router: Router, public http: Http) { }
+
+  prescribe(event, username, paracetamol, panadol) {
+  event.preventDefault();
+  let content = {"paracetamol":paracetamol,
+                  "panadol":panadol};
+  let body = JSON.stringify(content);
+  this.http.post('https://leniks05:o5slpq7wvncki1jm@cherry-1958777.us-east-1.bonsaisearch.net/try/try/'+username, body, { headers: contentHeaders })
+    .subscribe(
+      response => {
+        localStorage.setItem('id_token', response.json().id_token);
+        this.router.navigate(['']);
+      },
+      error => {
+        alert(error.text());
+        console.log(error.text());
+      }
+    );
+}
+
+home(event) {
+  event.preventDefault();
+  this.router.navigate(['']);
+}
+  ngOnInit() {
+  }
+
+}
